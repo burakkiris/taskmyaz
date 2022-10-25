2eb5974da92902d53a2a081142ae80649fa8562d
burakkiris <116682707+burakkiris@users.noreply.github.com>
2022-10-25 23:48:41 +0300

    Add files via upload

---
+++ HAFTA_2_ODEVI/Hafta_2_odevı/EmployeeModifier.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _2.hafta
+{
+    internal class EmployeeModifier
+    {
+        List<Employee> employees;
+
+        public EmployeeModifier()
+        {
+            employees = new List<Employee>();
+        }
+
+        public void Create(Employee employee)
+        {
+            employees.Add(employee);
+            Console.WriteLine(" Item added ");
+        }
+
+        public void Delete(int id)
+        {
+            var deleteEmp = employees.FirstOrDefault(x => x._id == id);
+
+            if (deleteEmp == null)
+            {
+                Console.WriteLine(" Item not found ");
+
+            }
+
+
+
+
+            else
+            {
+                employees.Remove(deleteEmp);
+                Console.WriteLine(id + " Item deleted ");
+            }
+
+
+
+
+        }
+
+        public void ReadList()
+        {
+            foreach (var emp in employees)
+            {
+                Console.WriteLine("id: " + emp._id);
+            }
+        }
+
+    }
+}
---
+++ HAFTA_2_ODEVI/Hafta_2_odevı/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace _2.hafta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(1, "Jeff", "Bezos");
+            Employee employee1 = new Employee(2, "Elon", "Musk");
+            Employee employee2 = new Employee(3, "Mark", "Zuckerberg");
+            Employee employee3 = new Employee(4, "Bill", "Gates");
+
+
+            EmployeeModifier employeeManager = new EmployeeModifier();
+
+            employeeManager.Create(employee1);
+            employeeManager.Create(employee3);
+            employeeManager.Delete(3);
+            employeeManager.Delete(325);
+            employeeManager.ReadList();
+
+
+
+        }
+    }
+}
